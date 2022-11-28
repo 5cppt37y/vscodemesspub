b6d33e7dcd2523a43aa6bd658dec6e50cbd8ed38
5cppt37y <maks.szkola138@gmail.com>
2022-11-28 15:16:40 +0100

    init

---
+++ passwordgen.py
@@ -0,0 +1,3 @@
+import secrets;a=('0','1','2','3','4','5','6','7','8','9','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','@');m=[]
+for i in range(32):m.append(secrets.choice(a))
+print(''.join(m))
